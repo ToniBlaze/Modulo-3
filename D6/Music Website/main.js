46b7465017c56ef99cb2c3eb5e447426d393e2e2
Alessio <alessio.toninello@gmail.com>
2023-03-15 16:06:55 +0100

    esercizio finito!!

--- D6/Music Website/main.js
+++ D6/Music Website/main.js
@@ -1,8 +1,9 @@
-// Funzione per eliminare tutti i bottoni
+// Funzione per eliminare le righe quando clicchi bottone
 var deleteButtons = document.querySelectorAll(".delete-row");
 
 for (var i = 0; i < deleteButtons.length; i++) {
-    deleteButtons[i].addEventListener("click", function () {
-        this.closest("tr").remove();
-    });
-}
+    deleteButtons[i].addEventListener("click", function (event) {
+    var row = event.target.parentNode.parentNode;
+    row.remove();
+  });
+};
